bb46d1f85eb15c6f81ef07200d7eb4f928b184ab
Andrew Condon <andrew.condon@gmail.com>
2020-12-05 13:31:54 +0100

    WIP follow on to prev commit
    type checks

--- src/Main.purs
+++ src/Main.purs
@@ -7,40 +7,48 @@ import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Effect.Var (($=))
-import Prelude (Unit, bind, discard, unit, void, when, (==), ($), (<<<), (<>), (=<<))
+import Prelude (Unit, pure, bind, discard, unit, void, when, (==), ($), (<<<), (<>), (=<<))
 import WebSocket (Connection(..), Message(..), runMessage, runMessageEvent, runURL)
 import WebSocket.Server (newWebSocketServer) as ServerWS
 
+
 openHandler socket event = do
-    void $ trace event
-    log "onopen: Connection opened"
+  pure unit
+    -- void $ trace event
+    -- log "onopen: Connection opened"
 
-    -- log <<< runURL =<< get socket.url
+    -- -- log <<< runURL =<< get socket.url
 
-    log "onopen: Sending 'hello'"
-    socket.send (Message "hello")
+    -- log "onopen: Sending 'hello'"
+    -- socket.send (Message "hello")
 
-    log "onopen: Sending 'goodbye'"
-    socket.send (Message "goodbye")
+    -- log "onopen: Sending 'goodbye'"
+    -- socket.send (Message "goodbye")
 
 messageHandler socket event = do
-    void $ trace event
-    let received = runMessage (runMessageEvent event)
+  pure unit
+    -- void $ trace event
+    -- let received = runMessage (runMessageEvent event)
 
-    log $ "onmessage: Received '" <> received <> "'"
+    -- log $ "onmessage: Received '" <> received <> "'"
 
-    when (received == "goodbye") do
-      log "onmessage: closing connection"
-      socket.close
+    -- when (received == "goodbye") do
+    --   log "onmessage: closing connection"
+    --   socket.close
 
 closeHandler socket event = do
-    void $ trace event
-    log "onclose: Connection closed"
+  pure unit
+    -- void $ trace event
+    -- log "onclose: Connection closed"
+
+setHandlers socket = do
+  socket.onopen    $= openHandler socket
+  socket.onmessage $= messageHandler socket
+  socket.onclose   $= closeHandler socket
+
 
 main :: Effect Unit
 main = launchAff_ do
   (Connection socket) <- ServerWS.newWebSocketServer 8080
-  socket.onopen    $= (openHandler socket)
-  socket.onmessage $= (messageHandler socket)
-  socket.onclose   $= (closeHandler socket)
+  liftEffect $ setHandlers socket
   liftEffect $ log "🍝"
