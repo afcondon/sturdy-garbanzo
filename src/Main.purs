7d0b7294efd29fc982debcc43d62ba7c46ddb1bd
Andrew Condon <andrew.condon@gmail.com>
2020-12-06 13:36:38 +0100

    messages being sent, handlers being attached

--- src/Main.purs
+++ src/Main.purs
@@ -5,50 +5,61 @@ import Debug.Trace (trace)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
-import Effect.Console (log)
+import Effect.Class.Console (log)
 import Effect.Var (($=))
-import Prelude (Unit, pure, bind, discard, unit, void, when, (==), ($), (<<<), (<>), (=<<))
+import Node.Express.Types (Event)
+import Prelude (Unit, bind, discard, pure, unit, void, when, ($), (<<<), (<>), (=<<), (==))
+import Web.Socket.Event.MessageEvent (MessageEvent)
 import WebSocket (Connection(..), Message(..), runMessage, runMessageEvent, runURL)
 import WebSocket.Server (newWebSocketServer) as ServerWS
 
 
-openHandler socket event = do
+openHandler :: forall e. Connection -> e -> Effect Unit
+openHandler (Connection socket) event = do
+  trace { fn: "openHandler", event: event } \_ -> pure unit
   pure unit
-    -- void $ trace event
-    -- log "onopen: Connection opened"
+  -- void $ trace event
+  -- log "onopen: Connection opened"
 
-    -- -- log <<< runURL =<< get socket.url
+  -- -- log <<< runURL =<< get socket.url
 
-    -- log "onopen: Sending 'hello'"
-    -- socket.send (Message "hello")
+  -- log "onopen: Sending 'hello'"
+  -- socket.send (Message "hello")
 
-    -- log "onopen: Sending 'goodbye'"
-    -- socket.send (Message "goodbye")
+  -- log "onopen: Sending 'goodbye'"
+  -- socket.send (Message "goodbye")
 
-messageHandler socket event = do
+messageHandler :: MessageEvent -> Effect Unit
+messageHandler event = do
+  trace { fn: "messageHandler", event: event } \_ -> pure unit
   pure unit
-    -- void $ trace event
-    -- let received = runMessage (runMessageEvent event)
+  -- let received = runMessage (runMessageEvent event)
 
-    -- log $ "onmessage: Received '" <> received <> "'"
+  -- log $ "onmessage: Received '" <> received <> "'"
 
-    -- when (received == "goodbye") do
-    --   log "onmessage: closing connection"
-    --   socket.close
+  -- when (received == "goodbye") do
+  --   log "onmessage: closing connection"
+  --   socket.close
 
-closeHandler socket event = do
+closeHandler :: forall e. Connection -> e -> Effect Unit
+closeHandler (Connection socket) event = do
+  trace { fn: "messageHandler", event: event } \_ -> pure unit
   pure unit
     -- void $ trace event
     -- log "onclose: Connection closed"
 
-setHandlers socket = do
-  socket.onopen    $= openHandler socket
-  socket.onmessage $= messageHandler socket
-  socket.onclose   $= closeHandler socket
+setHandlers :: Connection -> Effect Unit
+setHandlers c@(Connection socket) = do
+  socket.onopen    $= openHandler c
+  socket.onmessage $= messageHandler
+  socket.onclose   $= closeHandler c
 
+sendMessage (Connection socket) msg = socket.send $ Message msg
 
 main :: Effect Unit
 main = launchAff_ do
-  (Connection socket) <- ServerWS.newWebSocketServer 8080
+  socket <- ServerWS.newWebSocketServer 8080
   liftEffect $ setHandlers socket
+  liftEffect $ socket `sendMessage` "foo"
+  liftEffect $ (\(Connection ws) msg -> ws.send (Message msg)) socket "bar" 
   liftEffect $ log "🍝"
--- src/WebSocket/Server/Server.js
+++ src/WebSocket/Server/Server.js
@@ -11,70 +11,79 @@ const ws = module.require('ws')
 const app = express()
 app.use(express.static(path.join(__dirname, '/public')))
 const server = createServer(app)
+const WebSocket = require('ws');
+
+// const wss = new WebSocket.Server({ port: 8080 });
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//   });
+
+//   ws.send('something');
+// });
 
 exports._newWebSocketServer = (function () {
+  return port =>
+    function (onError, onSuccess) {
+      server.listen(port, function () {
+        console.log(`Listening on http://localhost:${port}`)
+      })
+      const wss = new ws.Server({ server })
+      wss.on('connection', function (ws) {
+        onSuccess(makeConnection(ws))
+      })
 
-  return (port)  => function(onError, onSuccess) {
-    server.listen(port, function () {
-      console.log(`Listening on http://localhost:${port}`)
-    })
-    const socket = new ws.Server({ server })
-    socket.on('connection', function() {
-       onSuccess(makeConnection())
-    })
-  
-    function makeConnection () {
-      console.log('interior of makeConnection')
-      const getSocketProp = (prop) => () => { console.log(`getSocket[${prop}]`); socket[prop]}
-      const setSocketProp = (prop) => (v) => () => { console.log(`setSocket[${prop} = ${v}]`); socket[prop] = v; return {} }
-  
-      socket.on('message', function (message) {
-        console.log(`Received message ${message}`);
-      });
-    
-    
-      return {
-        setBinaryType: setSocketProp('binaryType'),
-        getBinaryType: getSocketProp('binaryType'),
-        getBufferedAmount: getSocketProp('bufferedAmount'),
-        setOnclose: setSocketProp('onclose'),
-        getOnclose: getSocketProp('onclose'),
-        setOnerror: setSocketProp('onerror'),
-        getOnerror: getSocketProp('onerror'),
-        setOnmessage: setSocketProp('onmessage'),
-        getOnmessage: getSocketProp('onmessage'),
-        setOnopen: setSocketProp('onopen'),
-        getOnopen: getSocketProp('onopen'),
-        setProtocol: setSocketProp('protocol'),
-        getProtocol: getSocketProp('protocol'),
-        getReadyState: getSocketProp('readyState'),
-        getUrl: getSocketProp('url'),
-        closeImpl: function (params) {
-          return function () {
-            if (params == null) {
-              socket.close()
-            } else if (params.reason == null) {
-              socket.close(params.code)
-            } else {
-              socket.close(params.code, params.reason)
+      function makeConnection (ws) {
+        console.log('makeConnection')
+
+        const getSocketProp = prop => () => socket[prop]
+        const setSocketProp = (prop) => (v) => () => {
+          ws.on(prop, v)
+        }
+
+        return {
+          setBinaryType: setSocketProp('binaryType'),
+          getBinaryType: getSocketProp('binaryType'),
+          getBufferedAmount: getSocketProp('bufferedAmount'),
+          setOnclose: setSocketProp('onclose'),
+          getOnclose: getSocketProp('onclose'),
+          setOnerror: setSocketProp('onerror'),
+          getOnerror: getSocketProp('onerror'),
+          setOnmessage: setSocketProp('onmessage'),
+          getOnmessage: getSocketProp('onmessage'),
+          setOnopen: setSocketProp('onopen'),
+          getOnopen: getSocketProp('onopen'),
+          setProtocol: setSocketProp('protocol'),
+          getProtocol: getSocketProp('protocol'),
+          getReadyState: getSocketProp('readyState'),
+          getUrl: getSocketProp('url'),
+          closeImpl: function (params) {
+            return function () {
+              if (params == null) {
+                ws.close()
+              } else if (params.reason == null) {
+                ws.close(params.code)
+              } else {
+                ws.close(params.code, params.reason)
+              }
+              return {}
             }
-            return {}
+          },
+          sendImpl: function (message) {
+            return function () {
+              ws.send(message)
+              return {}
+            }
+          },
+          getSocket: function () {
+            return ws
           }
-        },
-        sendImpl: function (message) {
-          return function () {
-            socket.send(message)
-            return {}
-          }
-        },
-        getSocket: function () {
-          return socket
         }
       }
+      return function (cancelError, onCancelerError, onCancelerSuccess) {
+        cancel()
+        onCancelerSuccess()
+      }
     }
-    return function (cancelError, onCancelerError, onCancelerSuccess) {
-      cancel();
-      onCancelerSuccess();
-    };
-  }
 })()
